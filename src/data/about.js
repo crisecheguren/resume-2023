326fd84b4239bf1123ca9803e48fe70aea9de958
Cris <crisecheguren@gmail.com>
2023-04-13 16:40:45 -0400

    optimized about page by creating an about component that contains the biography info and refactored the about component to map through that instead of having the text directly on the page

---
+++ src/data/about.js
@@ -0,0 +1,9 @@
+const about = {
+    paragraphs: [
+        "I embarked on a career in the technology industry in 2012 as a repair technician for PCs, laptops, and cell phones in Portland, Oregon. Since then, I have cultivated a deep passion for technology and I've had the privilege of working with distinguished companies such as Extensis, Altavian, SharpSpring, and Constant Contact, progressing from the role of Tech Support Engineer to Support Manager. Throughout this journey, I've refined my troubleshooting and critical thinking skills, evolving into a compassionate and effective leader. Notably, I've achieved these managerial positions without formal education in the field, demonstrating my unwavering dedication to never giving up, no matter how difficult the path ahead.  With a growing enthusiasm for technology, I recently decided to further my education by pursuing a degree in Computer Science, setting my sights on a future as a software engineer.",
+        "To display my expanding range of technical projects and development skills, I have developed this website as a comprehensive portfolio.",
+        "Beyond my professional pursuits, I am a skilled jazz pianist with classical training from the age of five. My interests also include challenging roguelike games, such as DCSS and Nethack, and a passion for science fiction and fantasy literature, with the entire cosmere series under my belt. This diverse blend of critical thinking, leadership, and creativity has shaped a well-rounded and rewarding career in the technology industry."
+    ]
+}
+
+export default about;
--- src/routes/about-me/about-me.component.jsx
+++ src/routes/about-me/about-me.component.jsx
@@ -2,6 +2,7 @@ import { AboutWrapper, StyledContainer, StyledHeader } from "./about-me.styles";
 import Image from "../../assets/PXL_20230402_154551933.jpg";
 import SocialIcon from "../../components/social-icon/social-icon.component";
 import socialMediaLinks from '../../data/social'
+import about from '../../data/about'
 
 const AboutMe = ({onClick}) => {
     return (
@@ -14,21 +15,14 @@ const AboutMe = ({onClick}) => {
                     </p>
                 </div>
                 <div className="right">
-                    <p>
-                    I embarked on a career in the technology industry in 2012 as a repair technician for PCs, laptops, and cell phones in Portland, Oregon. Since then, I have cultivated a deep passion for technology and I've had the privilege of working with distinguished companies such as Extensis, Altavian, SharpSpring, and Constant Contact, progressing from the role of Tech Support Engineer to Support Manager. Throughout this journey, I've refined my troubleshooting and critical thinking skills, evolving into a compassionate and effective leader. Notably, I've achieved these managerial positions without formal education in the field, demonstrating my unwavering dedication to never giving up, no matter how difficult the path ahead.  With a growing enthusiasm for technology, I recently decided to further my education by pursuing a degree in Computer Science, setting my sights on a future as a software engineer.
-                    </p>
-                    
-                    <p>
-                     To display my expanding range of technical projects and development skills, I have developed this website as a comprehensive portfolio. 
-                     </p>
-                     <p>
-                     Beyond my professional pursuits, I am a skilled jazz pianist with classical training from the age of five. My interests also include challenging roguelike games, such as DCSS and Nethack, and a passion for science fiction and fantasy literature, with the entire cosmere series under my belt. This diverse blend of critical thinking, leadership, and creativity has shaped a well-rounded and rewarding career in the technology industry.
-                    </p>
+                    {about.paragraphs.map((paragraph, index) => (
+                        <p key={index}>{paragraph}</p>
+                    ))}
                 </div>
             </AboutWrapper>
             <div className="socialWrapper">
-                {socialMediaLinks.map((link, index) => (
-                    <SocialIcon key={index} Icon={link.Icon} url={link.url} className="socialMediaBtn" />
+                {socialMediaLinks.map(({ Icon, url }, index) => (
+                    <SocialIcon key={index} Icon={Icon} url={url} className="socialMediaBtn" />
                 ))}
             </div>
         </StyledContainer>
